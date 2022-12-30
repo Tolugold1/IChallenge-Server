a269054ad56c7aa1e1ddccd25ecb52994641f17b
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-12-30 10:19:49 +0100

    Delete configFile.js

--- configFile.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "secretKey": "toluadelekesteve",
-    "MongoUrl": "mongodb://localhost:27017/ichallengeServer",
-    "github": {
-        clientId: "f57e713812e226de259d",
-        clientSecret: "2eb96e3f8b6c8e1a61be3bcb722a43a8c760ad0e"
-    }
-}
