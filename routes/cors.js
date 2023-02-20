e9d804e05016b91cb1dc2a03a6f251db00393900
Tolugold <toluadelekesteve@gmail.com>
2023-02-20 12:45:48 +0100

    update

--- routes/cors.js
+++ routes/cors.js
@@ -1,11 +1,11 @@
 const cors = require("cors");
 
-const urls = ["http://localhost:3000", "https://localhost:3443", "http://localhost:3001", "https://challenge-tolugold1.vercel.app", "https://coral-fish-vest.cyclic.app/"];
+const urls = ["http://localhost:3000", "https://localhost:3443", "http://localhost:3001", "https://challenge-tolugold1.vercel.app", "https://coral-fish-vest.cyclic.app"];
 
 const corsDelegate = (req, cb) => {
     var corsOption;
-    console.log(req.header('Origin'));
-    if (urls.indexOf(req.header("Origin")) !== -1) {
+    console.log(req.header('Access-Control-Allow-Origin'));
+    if (urls.indexOf(req.header("Access-Control-Allow-Origin")) !== -1) {
         corsOption = { origin: true }
     } else {
         corsOption = { option: false }
