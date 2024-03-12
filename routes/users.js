898852ed9d433f37bed5df989b9cd1ddda81487e
Tolulope Adeleke <toluadelekesteve@gmail.com>
2024-03-12 09:06:08 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -83,7 +83,7 @@ router.post("/signin", cors.corsWithOption,  (req, res, next) => {
 router.post("/logout", (req, res, next) => {
   req.logOut((err) => {
     if (err) { return next(err) }
-    res.json({status: "https://challengedeveloper.netlify.app/"}) // "https://challenge-umber-six.vercel.app/"
+    res.json({status: "https://challengedeveloper.vercel.app/"}) // "https://challenge-umber-six.vercel.app/"
   })
 })
 
